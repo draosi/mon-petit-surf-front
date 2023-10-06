2c12c3c435511e90b7c26bddf91e1e4214ec1871
draosi <draosi.dev@gmail.com>
2023-10-06 09:17:51 +0200

    reset du projet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<style scoped>
+@import "@/assets/scss/views/home.scss";
+</style>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="test">Home Page Page</h1>
+  <p class="lorem">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis quis
+    nisi tempore, amet magnam nam enim maxime, aspernatur ab ullam aut iusto.
+    Cumque distinctio dolorum sed? Consequatur, modi at!
+  </p>
 </template>
+
+<script></script>
