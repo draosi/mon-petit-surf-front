af2dd1b53e511652612db6fd26177baa384e4413
draosi <draosi.dev@gmail.com>
2023-10-18 16:53:39 +0200

    home page en cours

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,25 @@
+<style lang="scss" scoped>
+@import "@/assets/scss/components/homeCard.scss";
+</style>
+
+<template>
+  <div class="card">
+    <h1 class="card__title">LACANAU-OCEAN</h1>
+    <div class="card__infos">
+      <div class="informations">
+        <div class="informations__txt">Région</div>
+        <div class="informations__txt">Gironde</div>
+      </div>
+      <div class="informations">
+        <div class="informations__txt">Taille moyenne des vagues</div>
+        <div class="informations__txt">1,5 m</div>
+      </div>
+      <div class="informations">
+        <div class="informations__txt">Temps de période moyen</div>
+        <div class="informations__txt">11 s</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,24 +3,39 @@
 </style>
 
 <template>
-    <Header />
-    <h1 class="test">Home Page Page</h1>
-    <p class="lorem">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis quis
-      nisi tempore, amet magnam nam enim maxime, aspernatur ab ullam aut iusto.
-      Cumque distinctio dolorum sed? Consequatur, modi at!
-    </p>
-    <Footer />
+  <Header />
+  <h1 class="test">Home Page Page</h1>
+  <p class="lorem">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facilis quis
+    nisi tempore, amet magnam nam enim maxime, aspernatur ab ullam aut iusto.
+    Cumque distinctio dolorum sed? Consequatur, modi at!
+  </p>
+  <main class="main">
+    <section class="sort">
+      <div></div>
+      <div></div>
+    </section>
+    <section class="spot">
+      <div class="spot__card">
+        <RouterLink to="/" class="spot__lien">
+          <HomeCard />
+        </RouterLink>
+      </div>
+    </section>
+  </main>
+  <Footer />
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue"
+import Footer from "@/components/Footer.vue";
+import HomeCard from "@/components/HomeCard.vue";
 
 export default {
   components: {
     Header,
     Footer,
+    HomeCard,
   },
 };
 </script>
