61c39fe86fe5d4e7bd9279778758e37efd8ad2b9
draosi <draosi.dev@gmail.com>
2023-10-19 11:51:59 +0200

    tentative de logique de trie

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,19 +7,19 @@
   <main class="main">
     <section class="sort">
       <div>
-        <select v-model="selected">
+        <select v-model="selectedHeight" @change="filterHeight">
           <option disabled value="">Taille moyenne</option>
           <option v-for="option in waveHeight" :value="option">
             {{ option }}
           </option>
         </select>
-        <select v-model="selected">
+        <select v-model="selectedPeriod" @change="log">
           <option disabled value="">Période moyenne</option>
           <option v-for="option in period" :value="option">
             {{ option }}
           </option>
         </select>
-        <select v-model="selected">
+        <select v-model="selectedRegion" @change="log">
           <option disabled value="">Selectionnez une région</option>
           <option v-for="option in regionEnDures" :value="option">
             {{ option }}
@@ -67,14 +67,16 @@ export default {
   },
   data() {
     return {
-      // Permet que 'Selectionnez une région' soit affiché par défaut
-      selected: "",
+      // Permet que le premier <option> soit affiché par défaut
+      selectedHeight: "",
+      selectedPeriod: "",
+      selectedRegion: "",
       waveHeight: [
         "vague < 0,9 m",
         "0,8 m < vague < 1,3 m",
         "1,2 m < vague < 1,7 m",
         "1,6 m < vague < 2,2 m",
-        "2,1 m < vague"
+        "2,1 m < vague",
       ],
       period: [
         "période < 7 s",
@@ -82,16 +84,17 @@ export default {
         "8 s < période < 11 s",
         "10 s < période < 13 s",
         "12 s < période < 15 s",
-        "14 s < période"
+        "14 s < période",
       ],
       region: [],
       spots: [],
+      filteredSpots: [],
       spotEnDur: [
         {
           id: 1,
           nom: "Lacanau-océan",
           region: "Gironde",
-          vague: 1.5,
+          vague: 0.8,
           periode: 11,
         },
         {
@@ -147,6 +150,29 @@ export default {
     cardBackground(i) {
       return i % 2 === 0;
     },
+    log() {
+      console.log(this.selectedHeight, this.selectedPeriod, this.selectedRegion);
+    },
+    filterHeight() {
+      if (this.selectedHeight === "vague < 0,9 m") {
+        this.filteredSpots = this.spotEnDur.filter((el) => {
+          el.vague < 0.9
+        })
+        console.log(this.filteredSpots);
+      }
+    },
+    filterPeriod() {
+
+    },
+    filterregion() {
+
+    }
   },
 };
 </script>
+
+<!-- "vague < 0,9 m",
+"0,8 m < vague < 1,3 m",
+"1,2 m < vague < 1,7 m",
+"1,6 m < vague < 2,2 m",
+"2,1 m < vague" -->
