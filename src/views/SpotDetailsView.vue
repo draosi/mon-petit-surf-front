850504961cc49880b13bb3581a95fa5905b6cfa7
draosi <draosi.dev@gmail.com>
2023-10-28 15:58:06 +0200

    création des méthode pour récupérer le spot et ses données météo, vague et vent

--- src/views/SpotDetailsView.vue
+++ src/views/SpotDetailsView.vue
@@ -3,50 +3,68 @@
 </style>
 
 <template>
-    <Header />
-    <main class="main">
-      <section class="infos">
-        <h1></h1>
-        <div>
-          <div></div>
-          <div></div>
-        </div>
-      </section>
-      <section></section>
-    </main>
-    <Footer />
-  </template>
-  
-  <script>
-  import Header from "@/components/Header.vue";
-  import Footer from "@/components/Footer.vue";
-  
-  export default {
-    data() {
-      return {
-        spotInfos: []
-      }
-    },
-    components: {
-      Header,
-      Footer,
-    },
-    methods: {
-      async fetchSpot(id) {
-        const res = await fetch(`https://localhost:7080/api/Spots/getSpot/${id}`)
-        const response = await res.json()
-        return response
-      },
+  <Header />
+  <main class="main">
+    <section class="infos">
+      <h1></h1>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+    <section></section>
+  </main>
+  <Footer />
+</template>
 
-      async getWavesConditions(latitude, longitude) {},
-      async getWindConditions(latitude, longitude) {},
-      async getPeriodConditions(latitude, longitude) {},
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
-      async createSpotInfos(id) {}
+export default {
+  data() {
+    return {
+      spotInfos: [],
+    };
+  },
+  components: {
+    Header,
+    Footer,
+  },
+  methods: {
+    async fetchSpot(id) {
+      const res = await fetch(`https://localhost:7080/api/Spots/getSpot/${id}`);
+      const response = await res.json();
+      return response;
     },
-    mounted() {
-      const spotId = this.$route.params
-      console.log(spotId);
-    }
-  };
-  </script>
+
+    async getWavesConditions(latitude, longitude) {
+      const res = await fetch(
+        `https://marine-api.open-meteo.com/v1/marine?latitude=${latitude}&longitude=${longitude}&hourly=wave_height,wave_direction,wave_period&timezone=GMT`
+      );
+      const response = await res.json();
+      return response;
+    },
+    async getWindConditions(latitude, longitude) {
+      const res = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=windspeed_10m,winddirection_10m`
+      );
+      const response = await res.json();
+      return response;
+    },
+    async getMeteo(latitude, longitude) {
+      const res = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=windspeed_10m,winddirection_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum`
+      );
+      const response = await res.json();
+      return response;
+    },
+
+    async createSpotInfos(id) {},
+  },
+  mounted() {
+    const spotId = this.$route.params;
+    console.log(spotId);
+  },
+};
+</script>
