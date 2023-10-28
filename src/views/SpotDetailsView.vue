522cc2f31aa12577eff7eaa851398beb0595b93e
draosi <draosi.dev@gmail.com>
2023-10-28 15:48:59 +0200

    set up des méthodes

--- src/views/SpotDetailsView.vue
+++ src/views/SpotDetailsView.vue
@@ -22,9 +22,31 @@
   import Footer from "@/components/Footer.vue";
   
   export default {
+    data() {
+      return {
+        spotInfos: []
+      }
+    },
     components: {
       Header,
       Footer,
     },
+    methods: {
+      async fetchSpot(id) {
+        const res = await fetch(`https://localhost:7080/api/Spots/getSpot/${id}`)
+        const response = await res.json()
+        return response
+      },
+
+      async getWavesConditions(latitude, longitude) {},
+      async getWindConditions(latitude, longitude) {},
+      async getPeriodConditions(latitude, longitude) {},
+
+      async createSpotInfos(id) {}
+    },
+    mounted() {
+      const spotId = this.$route.params
+      console.log(spotId);
+    }
   };
   </script>
