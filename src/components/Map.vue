4864a25549803fd81d3ee99d83dee8efbcbe8602
draosi <draosi.dev@gmail.com>
2023-10-29 14:09:32 +0100

    installation de leaflet

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,7 +3,18 @@
 </style>
 
 <template>
-    <h1 class="test">test</h1>
+  <div id="map"></div>
 </template>
 
-<script></script>
+<script>
+export default {
+  mounted() {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+  },
+};
+</script>
