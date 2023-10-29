87c624f301fa14bb3b7839aa821869d1284dab3e
draosi <draosi.dev@gmail.com>
2023-10-29 16:17:50 +0100

    set up du premier graph

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,38 @@
 <style lang="scss" scoped>
-@import '@/assets/scss/components/chart.scss';
+@import "@/assets/scss/components/chart.scss";
 </style>
 
 <template>
-    <h1 class="test">Test</h1>
+  <canvas id="chart"></canvas>
 </template>
 
-<script></script>
+<script>
+import Chart from "chart.js/auto";
+
+export default {
+  mounted() {
+    const ctx = document.getElementById("chart");
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  },
+};
+</script>
