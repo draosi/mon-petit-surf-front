54227c8d5f6ddd1ede6a62b0cac5c2c63a69c8b4
draosi <draosi.dev@gmail.com>
2023-11-06 23:31:34 +0100

    set up de la branche et du html

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -3,26 +3,28 @@
 </style>
 
 <template>
-    <Header />
-    <div>
-      <h1 class="favorite">Page favoris</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus sed,
-        vitae est eius vero numquam at? Maxime totam, enim modi nemo numquam
-        deserunt rem molestias laudantium odit necessitatibus omnis sunt!
-      </p>
-    </div>
-    <Footer />
-  </template>
-  
-  <script>
-  import Header from "@/components/Header.vue";
-  import Footer from "@/components/Footer.vue";
-  
-  export default {
-    components: {
-      Header,
-      Footer,
-    },
-  };
-  </script>
+  <Header />
+  <main>
+    <h1></h1>
+    <ul>
+      <li>
+        <div></div>
+        <button></button>
+        <button></button>
+      </li>
+    </ul>
+  </main>
+  <Footer />
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
