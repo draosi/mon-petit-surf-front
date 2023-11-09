4e67bc66d29533fb0437d48bb7e133327512d647
draosi <draosi.dev@gmail.com>
2023-11-09 16:21:21 +0100

    protection routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,68 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import SpotDetailsView from '../views/SpotDetailsView.vue'
-import FavoritesView from '../views/FavoritesView.vue'
-import ProfileView from '../views/ProfileView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import SpotDetailsView from "../views/SpotDetailsView.vue";
+import FavoritesView from "../views/FavoritesView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/connexion',
-      name: 'connexion',
-      component: LoginView
+      path: "/connexion",
+      name: "connexion",
+      component: LoginView,
     },
     {
-      path: '/inscription',
-      name: 'inscription',
-      component: RegisterView
+      path: "/inscription",
+      name: "inscription",
+      component: RegisterView,
     },
     {
-      path: '/spot/details/:spotId',
-      name: 'spotDetails',
+      path: "/spot/details/:spotId",
+      name: "spotDetails",
       component: SpotDetailsView,
-      props: true
+      props: true,
     },
     {
-      path: '/favoris/:userId',
-      name: 'favoris',
-      component: FavoritesView
+      path: "/favoris/:userId",
+      name: "favoris",
+      component: FavoritesView,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/profile/:userId',
-      name: 'profile',
-      component: ProfileView
+      path: "/profile/:userId",
+      name: "profile",
+      component: ProfileView,
+      meta: {
+        requiresAuth: true,
+      },
     },
-  ]
-})
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      // User is authenticated, proceed to the route
+      next();
+    } else {
+      // User is not authenticated, redirect to login
+      next("/connexion");
+    }
+  } else {
+    // Non-protected route, allow access
+    next();
+  }
+});
+
+export default router;
